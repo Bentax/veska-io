563845942543421784c351494564593c952e89ed
Andrey Kuzin <bentaxrus@mail.ru>
2024-06-27 16:06:34 +0300

    Update exchanges_events_1h.sql

--- exchanges_events_1h.sql
+++ exchanges_events_1h.sql
@@ -1,4 +1,4 @@
-CREATE TABLE exchanges_events_1h
+CREATE TABLE IF NOT EXISTS exchanges_events_1h
 (
     event String,
     event_timestamp UInt64,
@@ -28,33 +28,138 @@ ENGINE = MergeTree()
 ORDER BY event_timestamp
 TTL event_timestamp + INTERVAL 40 DAY;
 
-message Event {
-	string event = 1;
-	uint64 event_timestamp = 2;
-	
-	string exchange = 3;
-	string market = 4;
-	string base = 5;
-	string quot = 6;
+-- Агрегация для события 'price'
+INSERT INTO exchanges_events_1h
+SELECT
+    'price' AS event,
+    toUnixTimestamp(toStartOfHour(event_time)) AS event_timestamp,
+    exchange,
+    market,
+    base,
+    quot,
+    MIN(price) AS price_low,
+    MAX(price) AS price_high,
+    FIRST_VALUE(price) AS price_open,
+    LAST_VALUE(price) AS price_close,
+    NULL AS volume_quot,
+    NULL AS volume_base,
+    NULL AS volume_base_sell_taker,
+    NULL AS volume_base_buy_taker,
+    NULL AS oi_open,
+    NULL AS trades_count,
+    NULL AS liquidations_sell_count,
+    NULL AS liquidations_buy_count,
+    NULL AS liquidations_sell_base_volume,
+    NULL AS liquidations_buy_base_volume,
+    NULL AS liquidations_sell_quot_volume,
+    NULL AS liquidations_buy_quot_volume,
+    toUnixTimestamp64Milli(now()) AS updated_timestamp
+FROM futures_trades_stream
+GROUP BY
+    exchange,
+    market,
+    base,
+    quot,
+    toStartOfHour(event_time);
 
-	optional double price_open = 7;
-	optional double price_close = 8;
-	optional double price_high = 9;
-	optional double price_low = 10;
+-- Агрегация для события 'volume'
+INSERT INTO exchanges_events_1h
+SELECT
+    'volume' AS event,
+    toUnixTimestamp(toStartOfHour(event_time)) AS event_timestamp,
+    exchange,
+    market,
+    base,
+    quot,
+    NULL AS price_low,
+    NULL AS price_high,
+    NULL AS price_open,
+    NULL AS price_close,
+    SUM(volume_quot) AS volume_quot,
+    SUM(volume_base) AS volume_base,
+    SUM(volume_base_sell_taker) AS volume_base_sell_taker,
+    SUM(volume_base_buy_taker) AS volume_base_buy_taker,
+    NULL AS oi_open,
+    NULL AS trades_count,
+    NULL AS liquidations_sell_count,
+    NULL AS liquidations_buy_count,
+    NULL AS liquidations_sell_base_volume,
+    NULL AS liquidations_buy_base_volume,
+    NULL AS liquidations_sell_quot_volume,
+    NULL AS liquidations_buy_quot_volume,
+    toUnixTimestamp64Milli(now()) AS updated_timestamp
+FROM futures_trades_stream
+GROUP BY
+    exchange,
+    market,
+    base,
+    quot,
+    toStartOfHour(event_time);
 
-	optional double volume_quot = 11;
-	optional double volume_base = 12;
-	optional double volume_base_sell_taker = 13;
-	optional double volume_base_buy_taker = 14;
+-- Агрегация для события 'trades'
+INSERT INTO exchanges_events_1h
+SELECT
+    'trades' AS event,
+    toUnixTimestamp(toStartOfHour(event_time)) AS event_timestamp,
+    exchange,
+    market,
+    base,
+    quot,
+    NULL AS price_low,
+    NULL AS price_high,
+    NULL AS price_open,
+    NULL AS price_close,
+    NULL AS volume_quot,
+    NULL AS volume_base,
+    NULL AS volume_base_sell_taker,
+    NULL AS volume_base_buy_taker,
+    NULL AS oi_open,
+    COUNT(*) AS trades_count,
+    NULL AS liquidations_sell_count,
+    NULL AS liquidations_buy_count,
+    NULL AS liquidations_sell_base_volume,
+    NULL AS liquidations_buy_base_volume,
+    NULL AS liquidations_sell_quot_volume,
+    NULL AS liquidations_buy_quot_volume,
+    toUnixTimestamp64Milli(now()) AS updated_timestamp
+FROM futures_trades_stream
+GROUP BY
+    exchange,
+    market,
+    base,
+    quot,
+    toStartOfHour(event_time);
 
-	optional double oi_open = 15;
-
-	optional int32 trades_count = 16;
-
-	optional int32 liquidations_sell_count = 17;
-	optional int32 liquidations_buy_count = 18;
-	optional double liquidations_sell_base_volume = 19;
-	optional double liquidations_buy_base_volume = 20;
-	optional double liquidations_sell_quot_volume = 21;
-	optional double liquidations_buy_quot_volume = 22;
-}
+-- Агрегация для события 'liquidations'
+INSERT INTO exchanges_events_1h
+SELECT
+    'liquidations' AS event,
+    toUnixTimestamp(toStartOfHour(event_time)) AS event_timestamp,
+    exchange,
+    market,
+    base,
+    quot,
+    NULL AS price_low,
+    NULL AS price_high,
+    NULL AS price_open,
+    NULL AS price_close,
+    NULL AS volume_quot,
+    NULL AS volume_base,
+    NULL AS volume_base_sell_taker,
+    NULL AS volume_base_buy_taker,
+    NULL AS oi_open,
+    NULL AS trades_count,
+    SUM(liquidations_sell_count) AS liquidations_sell_count,
+    SUM(liquidations_buy_count) AS liquidations_buy_count,
+    SUM(liquidations_sell_base_volume) AS liquidations_sell_base_volume,
+    SUM(liquidations_buy_base_volume) AS liquidations_buy_base_volume,
+    SUM(liquidations_sell_quot_volume) AS liquidations_sell_quot_volume,
+    SUM(liquidations_buy_quot_volume) AS liquidations_buy_quot_volume,
+    toUnixTimestamp64Milli(now()) AS updated_timestamp
+FROM futures_trades_stream
+GROUP BY
+    exchange,
+    market,
+    base,
+    quot,
+    toStartOfHour(event_time);
