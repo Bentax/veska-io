2fe594b0532cc4b05970eb4c83ef7edef14123a6
Andrey Kuzin <bentaxrus@mail.ru>
2024-06-27 15:55:55 +0300

    Update exchanges_events_1h.sql

--- exchanges_events_1h.sql
+++ exchanges_events_1h.sql
@@ -1,8 +1,32 @@
-syntax = "proto3";
-
-package exchanges_events;
-
-option go_package = "github.com/veska-io/streams-proto/streams;eeventspb";
+CREATE TABLE exchanges_events_1h
+(
+    event String,
+    event_timestamp UInt64,
+    exchange String,
+    market String,
+    base String,
+    quot String,
+    price_open Nullable(Float64),
+    price_close Nullable(Float64),
+    price_high Nullable(Float64),
+    price_low Nullable(Float64),
+    volume_quot Nullable(Float64),
+    volume_base Nullable(Float64),
+    volume_base_sell_taker Nullable(Float64),
+    volume_base_buy_taker Nullable(Float64),
+    oi_open Nullable(Float64),
+    trades_count Nullable(Int32),
+    liquidations_sell_count Nullable(Int32),
+    liquidations_buy_count Nullable(Int32),
+    liquidations_sell_base_volume Nullable(Float64),
+    liquidations_buy_base_volume Nullable(Float64),
+    liquidations_sell_quot_volume Nullable(Float64),
+    liquidations_buy_quot_volume Nullable(Float64),
+    updated_timestamp UInt64 DEFAULT toUnixTimestamp64Milli(now())
+) 
+ENGINE = MergeTree()
+ORDER BY event_timestamp
+TTL event_timestamp + INTERVAL 40 DAY;
 
 message Event {
 	string event = 1;
