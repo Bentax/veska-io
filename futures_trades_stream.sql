e783a85e8f191dc166a083b5a3af431361902027
Andrey Kuzin <bentaxrus@mail.ru>
2024-07-13 13:22:03 +0300

    Update futures_trades_stream.sql

--- futures_trades_stream.sql
+++ futures_trades_stream.sql
@@ -3,12 +3,10 @@ CREATE TABLE IF NOT EXISTS futures_trades_stream
 updated_at DateTime DEFAULT now(),
 trade_timestamp UInt64 NOT NULL,
 created_at_height Nullable(UInt64) DEFAULT NULL,
-
 trade_id Nullable(String) DEFAULT NULL,
 agg_id Nullable(String) DEFAULT NULL,
 start_trade_id Nullable(String) DEFAULT NULL,
 end_trade_id Nullable(String) DEFAULT NULL,
-
 exchange String NOT NULL,
 market String NOT NULL,
 base String NOT NULL,
@@ -21,5 +19,3 @@ is_buyer_maker Nullable(bool) DEFAULT NULL
 ) ENGINE = MergeTree
 PARTITION BY toYear(fromUnixTimestamp(trade_timestamp))
 ORDER BY (trade_timestamp, exchange, market)
-
-INSERT INTO futures_trades_stream VALUES
