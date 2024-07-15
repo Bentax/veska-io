aacda349c5a546100b2e10fce3a9505c34cde0a2
Andrey Kuzin <bentaxrus@mail.ru>
2024-07-15 16:44:00 +0300

    Update binance_futures_klines_1h.sql

--- binance_futures_klines_1h.sql
+++ binance_futures_klines_1h.sql
@@ -1,18 +1,18 @@
-DESCRIBE binance_futures_klines_1h
+symbol	String
+base	String
+quot	String
+kline_timestamp	UInt64
+open_time	UInt64
+close_time	UInt64
+open	String
+high	String
+low	String
+close	String
+volume	String
+trade_num	UInt64
+quot_asset_volume	String
+taker_buy_base_asset_volume	String
+taker_buy_quot_asset_volume	String
+updated_timestamp	UInt64
 
-symbol	String					
-close	String					
-volume	String					
-trade_num	UInt64					
-quot_asset_volume	String					
-taker_buy_base_asset_volume	String					
-taker_buy_quot_asset_volume	String					
-updated_timestamp	UInt64	DEFAULT	toUnixTimestamp64Milli(now64(3))			
-base	String					
-quot	String					
-kline_timestamp	UInt64					
-open_time	UInt64					
-close_time	UInt64					
-open	String					
-high	String					
-low	String					
+########### DESCRIBE binance_futures_klines_1h
