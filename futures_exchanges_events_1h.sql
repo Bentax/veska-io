919187844f7d11556a9c992c192a6c467d2b8450
Andrey Kuzin <bentaxrus@mail.ru>
2024-07-17 15:52:05 +0300

    Update futures_exchanges_events_1h.sql

--- futures_exchanges_events_1h.sql
+++ futures_exchanges_events_1h.sql
@@ -54,3 +54,137 @@ CREATE TABLE IF NOT EXISTS exchanges_events_1h
 ENGINE = MergeTree()
 ORDER BY (event_timestamp, exchange, market)
 TTL toDate(updated_timestamp/1000) + INTERVAL 40 DAY;
+
+SELECT
+    'price' AS event,
+    kline_timestamp AS event_timestamp,
+    'binance' AS exchange,
+    concat(lower(base),'-usd') AS market,
+    lower(base) AS base,
+    'usd' AS quot,
+    anyLast(toFloat64(open)) AS price_open,
+    anyLast(toFloat64(close)) AS price_close,
+    anyLast(toFloat64(high)) AS price_high,
+    anyLast(toFloat64(low)) AS price_low,
+    NULL AS volume_base,
+    NULL AS volume_quot,
+    NULL AS volume_base_buy_taker,
+    NULL AS volume_quot_buy_taker,
+    NULL AS volume_base_sell_taker,
+    NULL AS volume_quot_sell_taker,
+    NULL AS oi_open,
+    NULL AS trades_count,
+    NULL AS liquidations_shorts_count,
+    NULL AS liquidations_longs_count,
+    NULL AS liquidations_shorts_base_volume,
+    NULL AS liquidations_longs_base_volume,
+    NULL AS liquidations_shorts_quot_volume,
+    NULL AS liquidations_longs_quot_volume,
+    NULL AS funding_rate,
+    toUnixTimestamp64Milli(now64(3)) AS updated_timestamp
+FROM binance_futures_klines_1h
+WHERE kline_timestamp >= (toUnixTimestamp64Milli(now64(3)) - 3600000)
+GROUP BY kline_timestamp, exchange, market, base, quot
+ORDER BY updated_timestamp DESC
+
+UNION ALL
+
+SELECT
+    'volume' AS event,
+    kline_timestamp AS event_timestamp,
+    'binance' AS exchange,
+    concat(lower(base),'-usd') AS market,
+    lower(base) AS base,
+    'usd' AS quot,
+    NULL AS price_open,
+    NULL AS price_close,
+    NULL AS price_high,
+    NULL AS price_low,
+    anyLast(toUInt64OrNull(volume)) AS volume_base,
+    anyLast(toFloat64(quot_asset_volume)) AS volume_quot,
+    anyLast(toUInt64OrNull(taker_buy_base_asset_volume)) AS volume_base_buy_taker,
+    anyLast(toFloat64(taker_buy_quot_asset_volume)) AS volume_quot_buy_taker,
+    anyLast(toFloat64(volume) - toFloat64(taker_buy_base_asset_volume)) AS volume_base_sell_taker,
+    anyLast(toFloat64(quot_asset_volume) - toFloat64(taker_buy_quot_asset_volume)) AS volume_quot_sell_taker,
+    NULL AS oi_open,
+    NULL AS trades_count,
+    NULL AS liquidations_shorts_count,
+    NULL AS liquidations_longs_count,
+    NULL AS liquidations_shorts_base_volume,
+    NULL AS liquidations_longs_base_volume,
+    NULL AS liquidations_shorts_quot_volume,
+    NULL AS liquidations_longs_quot_volume,
+    NULL AS funding_rate,
+    toUnixTimestamp64Milli(now64(3)) AS updated_timestamp
+FROM binance_futures_klines_1h
+WHERE kline_timestamp >= (toUnixTimestamp64Milli(now64(3)) - 3600000)
+GROUP BY kline_timestamp, exchange, market, base, quot
+ORDER BY updated_timestamp DESC
+
+UNION ALL
+
+SELECT
+    'trades' AS event,
+    kline_timestamp AS event_timestamp,
+    'binance' AS exchange,
+    concat(lower(base),'-usd') AS market,
+    lower(base) AS base,
+    'usd' AS quot,
+    NULL AS price_open,
+    NULL AS price_close,
+    NULL AS price_high,
+    NULL AS price_low,
+    NULL AS volume_quot,
+    NULL AS volume_base,
+    NULL AS volume_base_buy_taker,
+    NULL AS volume_quot_buy_taker,
+    NULL AS volume_base_sell_taker,
+    NULL AS volume_quot_sell_taker,
+    NULL AS oi_open,
+    anyLast(trade_num) AS trades_count,
+    NULL AS liquidations_shorts_count,
+    NULL AS liquidations_longs_count,
+    NULL AS liquidations_shorts_base_volume,
+    NULL AS liquidations_longs_base_volume,
+    NULL AS liquidations_shorts_quot_volume,
+    NULL AS liquidations_longs_quot_volume,
+    NULL AS funding_rate,
+    toUnixTimestamp64Milli(now64(3)) AS updated_timestamp
+FROM binance_futures_klines_1h
+WHERE kline_timestamp >= (toUnixTimestamp64Milli(now64(3)) - 3600000)
+GROUP BY kline_timestamp, exchange, market, base, quot
+ORDER BY updated_timestamp DESC
+
+UNION ALL
+
+SELECT
+    'funding' AS event,
+    funding_timestamp AS event_timestamp,
+    'binance' AS exchange,
+    concat(lower(base),'-usd') AS market,
+    lower(base) AS base,
+    'usd' AS quot,
+    NULL AS price_open,
+    NULL AS price_close,
+    NULL AS price_high,
+    NULL AS price_low,
+    NULL AS volume_quot,
+    NULL AS volume_base,
+    NULL AS volume_base_buy_taker,
+    NULL AS volume_quot_buy_taker,
+    NULL AS volume_base_sell_taker,
+    NULL AS volume_quot_sell_taker,
+    NULL AS oi_open,
+    NULL AS trades_count,
+    NULL AS liquidations_shorts_count,
+    NULL AS liquidations_longs_count,
+    NULL AS liquidations_shorts_base_volume,
+    NULL AS liquidations_longs_base_volume,
+    NULL AS liquidations_shorts_quot_volume,
+    NULL AS liquidations_longs_quot_volume,
+    anyLast(toFloat64(rate)) AS funding_rate,
+    toUnixTimestamp64Milli(now64(3)) AS updated_timestamp
+FROM binance_futures_funding_1h
+WHERE funding_timestamp >= (toUnixTimestamp64Milli(now64(3)) - 3600000)
+GROUP BY funding_timestamp, exchange, market, base, quot
+ORDER BY updated_timestamp DESC
