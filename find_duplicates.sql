837148df882953f928017b41dd4e2bcc4e8939f2
Andrey Kuzin <bentaxrus@mail.ru>
2024-07-26 16:37:52 +0300

    Create find_duplicates.sql

---
+++ find_duplicates.sql
@@ -0,0 +1,14 @@
+SELECT count(*)
+FROM binance_futures_klines_1h
+--WHERE kline_timestamp >= (toUnixTimestamp64Milli(now64(3)) - 3600000)
+GROUP BY updated_timestamp, kline_timestamp, base, quot
+HAVING count(*) > 1
+-- result NULL
+######
+SELECT count(*)
+FROM binance_futures_klines_1h
+--WHERE kline_timestamp >= (toUnixTimestamp64Milli(now64(3)) - 3600000)
+GROUP BY updated_timestamp, base, quot
+HAVING count(*) > 1
+-- result NOT NULL
+######
