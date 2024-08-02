0e2691e8a8bb0cea765f4603c2f7b4c81f9f674a
Andrey Kuzin <bentaxrus@mail.ru>
2024-08-02 14:37:44 +0300

    Create _ratio_by_tokens.sql

---
+++ _ratio_by_tokens.sql
@@ -0,0 +1,36 @@
+with exchange_data_frame as (
+    SELECT
+        toDateTime(agg_timestamp/1000) as datetime,
+        exchange, 
+        market,
+        any((volume_base_buy_taker - volume_base_sell_taker) / NULLIF(greatest(volume_base_buy_taker, volume_base_sell_taker), 0)) as _volume_base
+    from aggregates_1h
+
+    where
+        market in (${market})
+        AND exchange = '${exchange}'
+        AND toDateTime(agg_timestamp/1000) >= date_sub(hour,2*${window_size},$__fromTime)
+        AND toDateTime(agg_timestamp/1000) <=  $__toTime 
+        
+    group by agg_timestamp, exchange, market
+),
+
+exchange_data_frame_by_interval as (
+    SELECT
+        exchange,
+        market,
+        groupArray(_volume_base)[1] as __volume_base,
+        datetime as grouping_datetime
+
+    FROM
+        exchange_data_frame
+    GROUP BY grouping_datetime, exchange, market
+)
+
+select
+    grouping_datetime as time,
+    market,
+    anyLast(__volume_base) OVER (PARTITION BY market,exchange ORDER BY grouping_datetime ASC Range BETWEEN toUInt64((${window_size}-1)*60*60) PRECEDING AND CURRENT ROW) AS "_"
+from  exchange_data_frame_by_interval
+
+order by time asc;
