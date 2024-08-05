ee8d6dba856376ae7c60776599ce4d2c9f49765e
Andrey Kuzin <bentaxrus@mail.ru>
2024-08-05 19:19:58 +0300

    Update _ratio_by_tokens.sql

--- _ratio_by_tokens.sql
+++ _ratio_by_tokens.sql
@@ -1,37 +1,36 @@
-with exchange_data_frame as (
+WITH exchange_data_frame AS (
     SELECT
-        toDateTime(agg_timestamp/1000) as datetime,
-        exchange, 
+        toDateTime(agg_timestamp / 1000) AS datetime,
+        exchange,
         market,
         groupArray(volume_base_buy_taker)[1] AS vbbt,
         groupArray(volume_base_sell_taker)[1] AS vbst
-    from aggregates_1h
-
-    where
-        market in (${market})
+    FROM aggregates_1h
+    WHERE
+        market IN (${market})
         AND exchange = '${exchange}'
-        AND toDateTime(agg_timestamp/1000) >= date_sub(hour,2*${window_size},$__fromTime)
-        AND toDateTime(agg_timestamp/1000) <=  $__toTime 
-        
-    group by agg_timestamp, exchange, market
+        AND toDateTime(agg_timestamp / 1000) >= date_sub(hour, 2 * ${window_size}, $__fromTime)
+        AND toDateTime(agg_timestamp / 1000) <= $__toTime
+    GROUP BY datetime, exchange, market
 ),
 
-exchange_data_frame_by_interval as (
+exchange_data_frame_by_interval AS (
     SELECT
         exchange,
         market,
-        sum((vbbt-vbst)/(greatest(vbbt, vbst))) as __volume_base,
-        datetime as grouping_datetime
-
-    FROM
-        exchange_data_frame
+        datetime AS grouping_datetime,
+        sum((vbbt - vbst) / greatest(vbbt, vbst)) AS __volume_base
+    FROM exchange_data_frame
     GROUP BY grouping_datetime, exchange, market
 )
 
-select
-    grouping_datetime as time,
+SELECT
+    grouping_datetime AS time,
     market,
-    anyLast(__volume_base) OVER (PARTITION BY market,exchange ORDER BY grouping_datetime ASC Range BETWEEN toUInt64((${window_size}-1)*60*60) PRECEDING AND CURRENT ROW) AS "_"
-from  exchange_data_frame_by_interval
-
-order by time asc;
+    anyLast(__volume_base) OVER (
+        PARTITION BY market, exchange
+        ORDER BY grouping_datetime ASC
+        RANGE BETWEEN toUInt64((${window_size} - 1) * 60 * 60) PRECEDING AND CURRENT ROW
+    ) AS "_"
+FROM exchange_data_frame_by_interval
+ORDER BY time;
