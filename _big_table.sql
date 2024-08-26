6adec2dcadf900ede609bb157c80ae3be3013f11
Andrey Kuzin <bentaxrus@mail.ru>
2024-08-26 17:20:11 +0300

    Update _big_table.sql

--- _big_table.sql
+++ _big_table.sql
@@ -1,45 +1,38 @@
-WITH pre_load AS (
-    SELECT
-        toDateTime(agg_timestamp / 1000) AS grouping_datetime,
-        exchange, 
-        market,
-        toDecimal32(anyLast(IF(funding_rate != 0, funding_rate * 100, NULL)), 8) AS rate,
-        anyLast(IF(price_open != 0, price_open, NULL)) AS price,
-        MIN(IF(price_low != 0, price_low, NULL)) AS min_price,
-        MAX(IF(price_high != 0, price_high, NULL)) AS max_price
-    FROM public.aggregates_1h
-    WHERE
-        exchange = 'binance'
-    GROUP BY grouping_datetime, exchange, market
-),
-max_date AS (
-    SELECT 
-        MAX(toDateTime(agg_timestamp / 1000)) AS m_date,
-        IF(
-            date_trunc('hour', toDateTime(now())) > MAX(toDateTime(agg_timestamp / 1000)), 
-            MAX(toDateTime(agg_timestamp / 1000)),
-            toStartOfInterval(date_trunc('hour', toDateTime(now())), interval IF(exchange = 'dydx', 1, 8) hour)
-        ) AS to_filtDate,
-        MAX(IF(price_open != 0, toDateTime(agg_timestamp / 1000), NULL)) AS max_price_dt_1,
-        IF(
-            date_trunc('hour', toDateTime(now())) > MAX(IF(price_open != 0, toDateTime(agg_timestamp / 1000), NULL)), 
-            MAX(IF(price_open != 0, toDateTime(agg_timestamp / 1000), NULL)),
-            toStartOfInterval(date_trunc('hour', toDateTime(now())), interval IF(exchange = 'dydx', 1, 8) hour)
-        ) AS max_price_dt
-    FROM public.aggregates_1h 
-    WHERE exchange = 'binance'
-),
-stats2 AS (
-    SELECT  
-        market AS m2, 
-        MAX(price_open) AS now_price
-    FROM public.aggregates_1h
-    WHERE 
-        price_open != 0 
-        AND exchange = 'binance' 
-        AND toDateTime(agg_timestamp / 1000) = (SELECT max_price_dt FROM max_date)
-    GROUP BY market
-)
+WITH 
+    pre_load AS (
+        SELECT
+            toDateTime(agg_timestamp / 1000) AS grouping_datetime,
+            exchange, 
+            market,
+            toDecimal32(anyLast(IF(funding_rate != 0, funding_rate * 100, NULL)), 8) AS rate,
+            anyLast(IF(price_open != 0, price_open, NULL)) AS price,
+            MIN(IF(price_low != 0, price_low, NULL)) AS min_price,
+            MAX(IF(price_high != 0, price_high, NULL)) AS max_price
+        FROM aggregates_1h
+        WHERE exchange = 'binance'
+        GROUP BY grouping_datetime, exchange, market
+    ),
+    max_date AS (
+        SELECT 
+            MAX(toDateTime(agg_timestamp / 1000)) AS m_date,
+            MAX(IF(price_open != 0, toDateTime(agg_timestamp / 1000), NULL)) AS max_price_dt_1
+        FROM aggregates_1h 
+        WHERE exchange = 'binance'
+    ),
+    interval_hours AS (
+        SELECT IF('${exchange}' = 'dydx', 1, 8) AS interval_hour
+    ),
+    stats2 AS (
+        SELECT  
+            market AS m2, 
+            MAX(price_open) AS now_price
+        FROM aggregates_1h
+        WHERE 
+            price_open != 0 
+            AND exchange = 'binance' 
+            AND toDateTime(agg_timestamp / 1000) = (SELECT max_price_dt_1 FROM max_date)
+        GROUP BY market
+    )
 SELECT 
     p.market, 
     p.grouping_datetime AS datetime, 
@@ -50,4 +43,10 @@ SELECT
     s2.m2 AS market2
 FROM pre_load p
 LEFT JOIN stats2 s2 ON s2.m2 = p.market
-WHERE p.grouping_datetime = (SELECT to_filtDate FROM max_date);
+JOIN max_date m ON 1=1
+JOIN interval_hours i ON 1=1
+WHERE p.grouping_datetime = IF(
+    date_trunc('hour', toDateTime(now())) > m.m_date, 
+    m.m_date,
+    toStartOfInterval(date_trunc('hour', toDateTime(now())), interval i.interval_hour hour)
+);
